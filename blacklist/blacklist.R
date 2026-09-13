89dc72e7f37293ea865f248b3b135bbca0a905a3
snsinfu <snsinfu@gmail.com>
2026-09-13 12:08:12 +0900

    Fix off-by-one in blacklist

--- blacklist/blacklist.R
+++ blacklist/blacklist.R
@@ -9,6 +9,15 @@ config_file <- "blacklist_config.tsv"
 config <- read.delim(config_file, stringsAsFactors = FALSE)
 ah <- AnnotationHub()
 
+# AnnotationHub excluderanges objects have off-by-one start coords.
+# https://github.com/dozmorovlab/excluderanges/issues/8
+as_granges_bed <- function(x) {
+    gr <- granges(x)
+    gr <- gr[end(gr) > start(gr)]
+    start(gr) <- start(gr) + 1L
+    gr
+}
+
 for (i in seq_len(nrow(config))) {
     genome <- config$genome[i]
     out_file <- config$output_file[i]
@@ -17,7 +26,7 @@ for (i in seq_len(nrow(config))) {
     ah_ids <- trimws(unlist(strsplit(raw_ids, "[,]+")))
 
     gr_list <- lapply(ah_ids, function(id) ah[[id]])
-    gr_clean <- lapply(gr_list, granges)
+    gr_clean <- lapply(gr_list, as_granges_bed)
     gr_unified <- reduce(do.call(c, gr_clean))
 
     export.bed(gr_unified, out_file)
