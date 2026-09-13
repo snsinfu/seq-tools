f789442194a3ab45081539620c5ab3f632336ca6
snsinfu <snsinfu@gmail.com>
2026-09-13 22:29:32 +0900

    Add nfcore_upload.bash

---
+++ nfcore_upload.bash
@@ -0,0 +1,29 @@
+#!/bin/bash -eu
+
+bucket="${S3_BUCKET_NAME:-}"
+
+while getopts b: opt; do
+    case "${opt}" in
+    b)  bucket="${OPTARG}"
+        ;;
+    *)  exit 1
+    esac
+done
+
+shift $((OPTIND - 1))
+
+if [[ -z "${bucket}" ]]; then
+    echo "Bucket name is not specified (-b name or S3_BUCKET_NAME env var)" >&2
+    exit 1
+fi
+
+archive_rule=(
+    -maxdepth 1
+    \( -name '*.config' -o -name '*.csv' -o -name 'out*.tar.zst' \)
+)
+
+for prefix in "$@"; do
+    find "${prefix}" "${archive_rule[@]}" \
+      | awk -v bucket="${bucket}" '{ print "cp " $1 " s3://" bucket "/" $1 }' \
+      | s5cmd run
+done
