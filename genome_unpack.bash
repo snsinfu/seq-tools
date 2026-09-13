e8b5f3f1dcc67a004e2e14ff4b19f49542fffc09
snsinfu <snsinfu@gmail.com>
2026-09-13 11:07:30 +0900

    Add nextflow config to use local genome
    `params { genome = 'abc' }` allows to use Genome/abc/genome.fa etc.

---
+++ genome_unpack.bash
@@ -0,0 +1,21 @@
+#!/bin/bash -eu
+
+case $# in
+0)  exit
+    ;;
+1)  :
+    ;;
+*)  printf "%s\0" "$@" | xargs -0 -P0 -L1 "$0"
+    exit
+esac
+
+cd "$1"
+echo "Unpacking $1 ..." >&2
+
+for file in *.tar.zst; do
+    tar xf "${file}"
+done
+
+for file in *.fa.zst; do
+    unzstd "${file}"
+done
